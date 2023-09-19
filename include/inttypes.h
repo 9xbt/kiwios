132d37f3db016282830651b642c76a57fc78b8ca
9xbt <109512837+9xbt@users.noreply.github.com>
2023-09-19 17:35:25 +0200

    A

--- include/inttypes.h
+++ include/inttypes.h
@@ -25,7 +25,6 @@ typedef signed int int32_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-
-#define size_t uint32_t
+typedef unsigned int size_t;
 
 #endif
