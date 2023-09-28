f260d39e456dba28e26a4840976b9a92c130c1d5
xrc2 <flippy632@gmail.com>
2023-09-28 15:30:49 +0200

    forgot to update the build number

--- src/sys/common.h
+++ src/sys/common.h
@@ -5,7 +5,7 @@
 
 #pragma once
 
-const char buildstr[] = "\nkiwios [build 230920]";
+const char buildstr[] = "\nkiwios [build 230928]";
 const char logostr[] = " _    _          _  ___  ____\n| | _(_)_      _(_)/ _ \\/ ___|\n| |/ / \\ \\ /\\ / / | | | \\___ \\\n|   <| |\\ V  V /| | |_| |___) |\n|_|\\_\\_| \\_/\\_/ |_|\\___/|____/\n";
 const char copyrightstr[] = "Copyright (c) 2023 kiwiorg\nAll rights reserved\n";
 const char licensestr[] = "This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under certain conditions.\nFor more info, see https://www.gnu.org/licenses.\n";
